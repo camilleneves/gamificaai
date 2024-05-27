35b728a5d65e1826e9231ebe292944acb0353eab
CAMILLE BRUNES NEVES <45657999878@info.local>
2024-05-27 08:28:41 -0300

    feat:Json server incluido

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
 
-function abreFechaMenu() {
+function abreFechaMenu() {    
     // Menu fechado - tem a classe menu-fechado
     // Menu aberto - não tem a classe menu-fechado
 
@@ -10,7 +10,7 @@ function abreFechaMenu() {
     // menu.classList.toggle("menu-fechado")
 
     // Se o menu contem a classe menu-fechado
-    if (menu.classList.contains("menu-fechado")) {
+    if ( menu.classList.contains("menu-fechado") ) {
         // Abrir o menu - remover a classe menu-fechado
         menu.classList.remove("menu-fechado")
 
@@ -33,6 +33,12 @@ function abreFechaMenu() {
     }
 }
 
+// if (window.innerWidth < 1300) {
+//     menu.classList.add("menu-fechado")
+//     iconeBarras.style.display = "inline"
+//     iconeX.style.display = "none"
+// }
+
 onresize = () => {
     // Abrir o menu - remover a classe menu-fechado
     menu.classList.remove("menu-fechado")
@@ -53,9 +59,9 @@ let banner = document.querySelector(".banner")
 // slides[2] -> terceiro-banner
 
 let slides = [
-    "primeiro-banner",
-    "segundo-banner",
-    "terceiro-banner"
+    "primeiro-banner", // 0
+    "segundo-banner",  // 1
+    "terceiro-banner"  // 2
 ]
 
 let slideAtual = 0
@@ -68,40 +74,68 @@ function mostrarProximoSlide() {
 
     if (slideAtual < 2) {
         // Somar 1 na variavel slideAtual
-        slideAtual++
-
+        slideAtual++        
     } else {
+        // Voltar para o primeiro banner
         slideAtual = 0
     }
 
     // Mostrar slide de acordo com o slide atual
     banner.classList.add(slides[slideAtual])
-
 }
 
-function mostrarProximoSlideAnterior() {
+function mostrarSlideAnterior() {
     // Remover o slide anterior
     banner.classList.remove(slides[slideAtual])
 
     if (slideAtual > 0) {
         // Subtrair 1 na variavel slideAtual
         slideAtual--
-
     } else {
+        // Voltar para o ultimo slide
         slideAtual = 2
     }
-
 
     // Mostrar slide de acordo com o slide atual
     banner.classList.add(slides[slideAtual])
 }
 
 function selecionarSlide(indiceSlide) {
-    // Remover o slide anterior
+    // Remove o slide atual
     banner.classList.remove(slides[slideAtual])
-
-    slideAtual = indiceSlide
     
-    // Mostrar slide de acordo com o slide atual
+    // Atualiza a variavel com o indice de slide selecionado
+    slideAtual = indiceSlide
+
+    // Mostra o slide selecionado e salvo na variavel slideAtual
     banner.classList.add(slides[slideAtual])
 }
+
+
+// Carregamento dinâmico dos cases
+let listaCases = [
+    
+]
+
+function renderizarCases() {
+    // Encontrar o elemento para inserir os cards
+    let containerCards = document.querySelector(".container-cards")
+
+    // Variavel para guardar o html dos cases montados
+    let template = ""
+
+    // Para cada case da listaCases
+    listaCases.forEach(cardCase => {
+        // Montar o html do card, passando os atributos do case
+        template += `<div class="card">
+            <img src=${ cardCase.imagem } alt="">
+            <p>${ cardCase.descricao }</p>
+            <button>Ver mais</button>
+        </div>`
+    })
+
+    // Inserir html dos cases montados no elemento container-cards
+    containerCards.innerHTML = template
+}
+
+
